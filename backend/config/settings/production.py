f9a4aed7f216091b20d247909b3083288dab0832
Gaurav Sethi <gauravsethi4627@gmail.com>
2026-05-29 02:46:03 +0530

    chore: configure production database settings and update default environment to production

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -3,15 +3,24 @@ import os
 
 import dj_database_url
 from decouple import config
+from pathlib import Path
 
 SECRET_KEY = config('SECRET_KEY', default='placeholder')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
 
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=config('DATABASE_URL', default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'))
+#     )
+# }
+
 DATABASES = {
     'default': dj_database_url.config(
-        default=config('DATABASE_URL', default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'))
+        default=config('DATABASE_URL', default='sqlite:///' + str(BASE_DIR / 'db.sqlite3')),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
